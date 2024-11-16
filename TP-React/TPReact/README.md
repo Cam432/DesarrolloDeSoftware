2c9fac89ee7acd4f94ec71b35d53e51cce61011b
Cam <108767205+Cam432@users.noreply.github.com>
2024-11-16 12:27:34 -0300

    Add files via upload

---
+++ TP-React/TPReact/README.md
@@ -0,0 +1,16 @@
+# Trabajo Práctico
+
+## Alumno
+
+- Corazza María Cecilia
+- Legajo 50230
+- 3k10
+- 2024
+
+## Ejecución
+
+Para clonar el repositorio, abra el bash de git y ejecute el siguiente comando:
+
+`git clone https://github.com/checorazza/Desarrollo-TPReact`
+
+Navegue a la carpeta donde se descargaron los archivos.
---
+++ TP-React/TPReact/src/App.tsx
@@ -0,0 +1,13 @@
+import { AppProduct } from "./components/AppProduct/AppProduct";
+import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter";
+import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect";
+import { FormComponent } from "./components/FormComponent/FormComponent";
+import { MiPrimerComponent } from "./components/MiPrimerComponent/MiPrimerComponent";
+
+export const App = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "2vh" }}>
+      <AppProduct />
+    </div>
+  );
+};
